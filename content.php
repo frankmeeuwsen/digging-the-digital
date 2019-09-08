695b55f0bff83d368d4b40bc7b1c383e9929e549
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-09-08 20:47:34 +0200

    frontpage fix

--- content.php
+++ content.php
@@ -29,7 +29,7 @@
 
 				<?php /* Only show featured image for Standard post and gallery post formats */ ?>
 				<?php if ( has_post_thumbnail() && in_array( get_post_format(), array( 'gallery', false ) ) ) : ?>
-					<a href="<?php the_permalink(); ?>" title="<?php echo independent_publisher_post_thumbnail_link_title(); ?>"><?php the_post_thumbnail( 'independent_publisher_post_thumbnail' ); ?></a>
+					<a href="<?php the_permalink(); ?>" title="<?php echo independent_publisher_post_thumbnail_link_title(); ?>"><?php the_post_thumbnail( 'independent_publisher_post_frontpage' ); ?></a>
 				<?php endif; ?>
 
 			<?php endif; ?>
