c6a4afad5dbc8107e43de13df7a2546a028530ee
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-10-02 13:33:20 +0200

    improve link on frontpage

--- functions.php
+++ functions.php
@@ -99,13 +99,35 @@ register_sidebar( array(
 	'after_title'   => '</h2>',
 ) );
 
-		/**
-		 * Enable Post Thumbnails
-		 */
-		add_theme_support( 'post-thumbnails' );
+/**
+ * Enable Post Thumbnails
+ */
+add_theme_support( 'post-thumbnails' );
 
-		/*
-		 * Add custom thumbnail size for use with featured images
-		 */
+/*
+	* Add custom thumbnail size for use with featured images
+	*/
 
-		add_image_size( 'independent_publisher_post_frontpage', 200, 200 );
+add_image_size( 'independent_publisher_post_frontpage', 200, 200 );
+
+function improved_trim_excerpt($text) {
+	global $post;
+	if ( '' == $text ) {
+		$text = get_the_content('');
+		$text = apply_filters('the_content', $text);
+		$text = str_replace('\]\]\>', ']]&gt;', $text);
+		$text = preg_replace('@<script[^>]*?>.*?</script>@si', '', $text);
+		$text = strip_tags($text, '<p>, <a>');
+		// $excerpt_length = 80;
+		$words = explode(' ', $text, $excerpt_length + 1);
+		if (count($words)> $excerpt_length) {
+			array_pop($words);
+			array_push($words, '[...]');
+			$text = implode(' ', $words);
+		}
+	}
+	return $text;
+}
+
+remove_filter('get_the_excerpt', 'wp_trim_excerpt');
+add_filter('get_the_excerpt', 'improved_trim_excerpt');
