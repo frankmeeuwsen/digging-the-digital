1f9218572ef41cf89e789b198fa70887ac54c1db
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-05-21 21:07:31 +0200

    updates

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -4,38 +4,13 @@
  * @since   Independent Publisher 1.0
  */
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemscope="itemscope" itemtype="http://schema.org/BlogPosting" itemprop="blogPost">
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemscope="itemscope"
+	itemtype="http://schema.org/BlogPosting" itemprop="blogPost">
 	<?php if ( has_post_thumbnail() && ! indieweb_publisher_has_full_width_featured_image() ) : ?>
-		<?php the_post_thumbnail( 'indieweb_publisher_post_thumbnail', array( 'itemprop' => 'image' ) ); ?>
+	<?php the_post_thumbnail( 'indieweb_publisher_post_thumbnail', array( 'itemprop' => 'image' ) ); ?>
 	<?php endif; ?>
 	<header>
-		<?php if ( indieweb_publisher_post_has_post_cover_title() ) : ?>
-			<h2 class="entry-title-meta">
-	  <span class="entry-title-meta-author">
-			<?php
-			if ( ! indieweb_publisher_categorized_blog() ) {
-				echo indieweb_publisher_entry_meta_author_prefix() . ' ';
-			}
-			indieweb_publisher_posted_author()
-			?>
-
-		</span>
-				<?php
-				if ( get_post_meta( get_the_ID(), 'indieweb_publisher_primary_category', true ) ) { // check for a custom field named 'indieweb_publisher_primary_category'
-					echo indieweb_publisher_entry_meta_category_prefix() . ' ' . get_post_meta( get_the_ID(), 'indieweb_publisher_primary_category', true ); // show the primary category as set in ACF
-				} elseif ( indieweb_publisher_categorized_blog() ) {
-					echo indieweb_publisher_entry_meta_category_prefix() . ' ' . indieweb_publisher_post_categories();
-				}
-				?>
-				<span class="entry-title-meta-post-date">
-				<span class="sep"> <?php echo apply_filters( 'indieweb_publisher_entry_meta_separator', '|' ); ?> </span>
-					<?php indieweb_publisher_posted_on_date(); ?>
-			</span>
-				<?php do_action( 'indieweb_publisher_entry_title_meta', $separator = ' | ' ); ?>
-			</h2>
-		<?php else : ?>
-			<?php if ( ! indieweb_publisher_option( 'show_author_card' ) ) { ?>
-			<h2 class="entry-title-meta">
+		<h2 class="entry-title-meta">
 			<span class="entry-title-meta-author">
 				<?php
 				if ( ! indieweb_publisher_categorized_blog() ) {
@@ -44,30 +19,28 @@
 				indieweb_publisher_posted_author()
 				?>
 			</span>
-				<?php
+			<?php
 				if ( indieweb_publisher_categorized_blog() ) {
 					echo indieweb_publisher_entry_meta_category_prefix() . ' ' . indieweb_publisher_post_categories();
 				}
 				?>
-				<span class="entry-title-meta-post-date">
-				<span class="sep"> <?php echo apply_filters( 'indieweb_publisher_entry_meta_separator', '|' ); ?> </span>
-					<?php indieweb_publisher_posted_on_date(); ?>
+			<span class="entry-title-meta-post-date">
+				<span class="sep"> <?php echo apply_filters( 'indieweb_publisher_entry_meta_separator', '|' ); ?>
+				</span>
+				<?php indieweb_publisher_posted_on_date(); ?>
 			</span>
-				<?php do_action( 'indieweb_publisher_entry_title_meta', $separator = ' | ' ); ?>
-			</h2>
-			<?php 
-			}
-				//$title = indieweb_publisher_get_the_title();
-				 $title = get_the_title();
+			<?php do_action( 'indieweb_publisher_entry_title_meta', $separator = ' | ' ); ?>
+		</h2>
+		<?php 
+			$title = get_the_title();
 			if ( ! empty( $title ) ) {
-				?>
-				<h1 class="entry-title p-name" itemprop="name"><?php echo $title; ?></h1>
-				<?php
-			}
-		endif;
-?>
+		?>
+		<h1 class="entry-title p-name" itemprop="name"><?php echo $title; ?></h1>
+		<?php 
+			}  
+		?>
 	</header>
-	
+
 	<?php the_content(); ?>
 	<?php indieweb_publisher_show_related_tags(); ?>
 	<?php indieweb_publisher_posted_author_bottom_card(); ?>
@@ -78,11 +51,11 @@
 
 		<?php if ( function_exists( 'wp_pagenavi' ) ) : // WP-PageNavi support ?>
 
-			<?php wp_pagenavi( array( 'type' => 'multipart' ) ); ?>
+		<?php wp_pagenavi( array( 'type' => 'multipart' ) ); ?>
 
 		<?php else : ?>
 
-			<?php
+		<?php
 			wp_link_pages(
 				array(
 					'before'           => '<div class="page-links-next-prev">',
@@ -93,7 +66,7 @@
 				)
 			);
 			?>
-			<?php
+		<?php
 			wp_link_pages(
 				array(
 					'before' => '<div class="page-links">' . __( 'Pages:', 'independent-publisher' ),
@@ -107,15 +80,15 @@
 
 
 		<?php if ( comments_open() && ! indieweb_publisher_hide_comments() ) : ?>
-			<div id="share-comment-button">
-				<button>
-					<i class="share-comment-icon"></i><?php echo indieweb_publisher_comments_call_to_action_text(); ?>
-				</button>
-			</div>
+		<div id="share-comment-button">
+			<button>
+				<i class="share-comment-icon"></i><?php echo indieweb_publisher_comments_call_to_action_text(); ?>
+			</button>
+		</div>
 		<?php endif; ?>
 
 		<?php edit_post_link( __( 'Edit', 'independent-publisher' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer>
 	<!-- .entry-meta -->
 
-</article><!-- #post-<?php the_ID(); ?> -->
+</article>
