677c0dab1b47166e49fe05e9dccdbd334dd54012
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-05-21 16:15:06 +0200

    error 2

--- functions.php
+++ functions.php
@@ -46,8 +46,8 @@ add_action( 'wp_enqueue_scripts', 'indieweb_publisher_stylesheet' );
 function indieweb_publisher_stylesheet() {
 	$parent_style = 'parent-style'; 
 	wp_enqueue_style( $parent_style, 
-		get_template_directory_uri() . '/css/default.min.css');
-		// wp_get_theme()->get('Version'));
+		get_template_directory_uri() . '/css/default.min.css',
+		wp_get_theme()->get('Version'));
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style )
